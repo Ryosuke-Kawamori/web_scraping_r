a35e039070043ed22726e1fa65d2e5e65b1a33b6
河盛亮介 <ryosuke@adsl-172-10-1-161.dsl.sndg02.sbcglobal.net>
2018-02-28 00:59:32 +0900

    comi

--- R/scrape_torikizoku.R
+++ R/scrape_torikizoku.R
@@ -1,15 +1,3 @@
-follow_links <- function(session, xpath){
-  session_len <- session %>% html_nodes(xpath=xpath) %>% seq_along()
-  duration <- rexp(n=session_len, rate=1)
-  session_len %>% map(., function(i){
-    Sys.sleep(duration[i])
-    follow_link(session, xpath=xpath, i=i)})
-}
-read_htmls <- function(urls){
-  map(urls, read_html)
-}
-
-
 torikizoku_top <- html_session(url = "https://sp.torikizoku.co.jp/list/")
 torikizoku_ken <- follow_links(torikizoku_top, "//li[@class = 'linkBox']/a")
 torikizoku_tiku <- map(torikizoku_ken, follow_links,  xpath="//li[@class = 'linkBox']/a") %>% flatten
